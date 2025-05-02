3a5f43d7d0621058e99f123d05759ab32abeeb1f
iSebDev <56876154@ceibal.edu.uy>
2025-05-02 16:33:24 -0300

    Actualizacion Interactividad Momo

--- scripts/event.js
+++ scripts/event.js
@@ -4,11 +4,17 @@ export class Handler {
 
         this.letterKeys = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "ñ", "z", "x", "c", "v", "b", "n", "m"];
         this.backspace = "Backspace"
+        
+        this.keyBoard = document.querySelectorAll(".key-btn");
 
         this.selected = null;
+        this.playingRow = null;
 
         this.keyPressHandler();
         this.letterPressHandler();
+        this.keyBoardHandler();
+
+        this.updateEvent();
     }
 
     keyPressHandler() {
@@ -25,9 +31,9 @@ export class Handler {
             const letters = document.querySelectorAll(`.enabled.playing .letter-column`);
 
             letters.forEach((i) => {
-                if (i.classList.contains("selected")) previous = i;
+                if (previous !== null && selected == null) selected = i;
 
-                if (previous !== null) selected = i;
+                if (i.classList.contains("selected")) previous = i;
             });
 
             this.letters.forEach((i) => i.classList.remove("selected") )
@@ -38,7 +44,7 @@ export class Handler {
         document.addEventListener("keydown", (e) => {
             this.updateEvent();
 
-            if (e.key === this.backspace) this.selected.textContent = "";
+            if (e.key === this.backspace) document.querySelector("#btn-1").click();
         });
     }
 
@@ -50,11 +56,58 @@ export class Handler {
                 this.letters.forEach((j) => j.classList.remove("selected") )
 
                 i.classList.add("selected");
+
+                this.updateEvent()
+            });
+        });
+    }
+
+    keyBoardHandler() {
+        this.keyBoard.forEach((i) => {
+            i.addEventListener("click", () => {
+                if (this.selected === null) return;
+                
+                var previous = null;
+                var selected = null;
+
+                const letters = document.querySelectorAll(`.enabled.playing .letter-column`);
+
+                switch (i.id) {
+                    case "btn-0": 
+                        break;
+                    case "btn-1": 
+                        this.updateEvent();
+                        this.selected.textContent = "";
+                        letters.forEach((i) => {
+                            if (!i.classList.contains("selected")) previous = i;
+                            else selected = previous;
+                        });        
+                        break;
+                    default:    
+                        this.updateEvent();
+                        this.selected.textContent = i.ariaLabel.toString().toUpperCase();
+                        letters.forEach((i) => {
+                            if (previous !== null && selected == null) selected = i;
+        
+                            if (i.classList.contains("selected")) previous = i;
+                        });        
+                        break;
+                }
+
+                this.letters.forEach((i) => i.classList.remove("selected") )
+                
+                selected.classList.add("selected");
             });
         });
     }
 
     updateEvent() {
         this.selected = document.querySelector(".letter-column.selected span");
+
+        this.playingRow = document.querySelector(".enabled.playing");
+
+        if (!(this.selected === null)) return;
+
+        this.selected = this.playingRow.querySelector(".letter-column.selected");
     }
 }
