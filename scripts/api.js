7cc2de437e995bc9ccf5732fc86aeb74c786e350
iSebDev <56876154@ceibal.edu.uy>
2025-05-05 22:39:14 -0300

    Interact.js

--- scripts/api.js
+++
@@ -1,9 +0,0 @@
-export default class WordleAPI {
-    constructor() {
-        this.word = null;
-        this.length = null;
-        this.max = null;
-        this.keyboardDiv = document.getElementById("keyboard-panel");
-        this.wordlePanel = document.getElementById("wordle-panel");
-    }
-}
